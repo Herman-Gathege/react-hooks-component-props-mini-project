b0765d1c59bbe082e980536b11b6341e392e1ab2
Herman-Gathege <siinamtechstudio@gmail.com>
2024-08-19 11:42:21 +0300

    added changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header";
+import About from "./About";
+import ArticleList from './ArticleList';
 
 console.log(blogData);
 
 function App() {
+  const blogPosts = [
+    {
+      title: "First Post",
+      date: "August 5, 2024",
+      preview: "This is the preview of the first post.",
+    },
+    {
+      title: "Second Post",
+      date: "August 6, 2024",
+      preview: "This is the preview of the second post.",
+    },
+  ];
+
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name="My Blog" />
+      <About
+        image="https://via.placeholder.com/215"
+        about="Welcome to my blog!"
+      />
+      <ArticleList posts={blogPosts} />
     </div>
   );
 }
